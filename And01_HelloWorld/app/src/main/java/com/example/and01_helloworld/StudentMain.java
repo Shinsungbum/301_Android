527a77deb231577c9ac287bd95e92a0195665dd5
SinSungBum <ssb97015364@gmail.com>
2022-07-12 17:46:47 +0900

    2022/07/12 Android

---
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/StudentMain.java
@@ -0,0 +1,13 @@
+package com.example.and01_helloworld;
+
+public class StudentMain {
+    //주석
+    //변수선언
+    int num1, num2;
+
+    public static void main(String[] args) {
+        StudentMain sm = new StudentMain();
+        sm.num1 = 10;
+        System.out.println(sm.num1);
+    }
+}
