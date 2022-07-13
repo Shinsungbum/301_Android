58b908b90918c54f85ddfdf83af3084bcfd2bc3c
SinSungBum <ssb97015364@gmail.com>
2022-07-13 18:16:22 +0900

    2022/07/13

--- And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/MainActivity.java
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.and01_helloworld;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
